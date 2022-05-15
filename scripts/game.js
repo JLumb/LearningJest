7ed80de437a2f54b037aeb4730b4b1f321dc7580
Josh Lumb <JoshLumby@hotmail.co.uk>
2022-05-15 13:08:31 +0000

    add newGame function to clear and reset scores and moves

---
+++ scripts/game.js
@@ -0,0 +1,19 @@
+let game = {
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+    score: 0,
+};
+
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+    showScore();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore };
